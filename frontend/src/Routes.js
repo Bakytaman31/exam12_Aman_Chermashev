cf54c11dd20942ac6c478f90f0c7553ddce7e04c
Aman Chermashev <bakytaman228337@gmail.com>
2020-04-18 12:17:50 +0600

    [FRONTEND]Created Register and Login

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
 
 const Routes = () => {
     return (
         <Switch>
+            <Route path="/register" exact component={Register}/>
+            <Route path="/login" exact component={Login}/>
             <Route path="/" exact />
         </Switch>
     );
